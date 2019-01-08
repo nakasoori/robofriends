bb69a6a782ee36a56f0eccaf42a43f09531ec71d
Seth Nakanishi <snakanishi@sandiego.edu>
2019-01-07 19:03:21 -0800

    Added files

---
+++ src/containers/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import SearchBox from '../components/SearchBox'
+import CardList from '../components/CardList'
+import { robots } from '../robots';
+import './App.css';
+import Scroll from '../components/Scroll'
+
+import { setSearchField, requestRobots } from '../actions'
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+//Using STATE
+//State is something that can change and affect our app
+//Usually lives within the parent component 
+class App extends Component {
+	componentDidMount() {
+		//console.log(this.props.store.getState())
+		this.props.onRequestRobots()
+	}
+
+	render() {
+		const { searchField, onSearchChange, robots, isPending } = this.props;
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
+		})	
+
+			return isPending ?
+			<h1>Loading</h1> :
+			(
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.props.onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>
+				</div>
+			);
+	}
+} 
+
+/*
+const App = () => {
+	return (
+		<div className='tc'>
+			<h1>RoboFriends</h1>
+			<SearchBox />
+			<CardList robots={robots}/>
+		</div>
+	);
+}
+*/
+export default connect(mapStateToProps, mapDispatchToProps)(App);
