5b5a030f0e55847bec9a763fc146b81a9490de3a
Seth Nakanishi <snakanishi@sandiego.edu>
2019-03-11 19:45:17 -0700

    Optimized and finalized into a PWA
    Testing and Reviews done through Lighthouse

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -23,7 +23,7 @@ class CounterButton extends Component {
 
 	render() {
 		return (
-			<button color={this.props.color} onClick={this.updateCount}>
+			<button style={{minHeight: '48px', minWidth: '48px'}}color={this.props.color} onClick={this.updateCount}>
 				Count: {this.state.count}
 			</button>
 		)
