7838b3ec411f8e5265257a6fad9c36eceb56fb90
Seth Nakanishi <snakanishi@sandiego.edu>
2019-03-11 19:14:42 -0700

    Updates

---
+++ precache-manifest.5d04989d281ae6de3a9c598086751e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "e6549d3493e44861c5be",
+    "url": "/robofriends/static/js/main.da526a77.chunk.js"
+  },
+  {
+    "revision": "ae7f903fd7b889ab839a",
+    "url": "/robofriends/static/js/2.fd9104e8.chunk.js"
+  },
+  {
+    "revision": "e6549d3493e44861c5be",
+    "url": "/robofriends/static/css/main.71c3a11c.chunk.css"
+  },
+  {
+    "revision": "ae7f903fd7b889ab839a",
+    "url": "/robofriends/static/css/2.f699bdf0.chunk.css"
+  },
+  {
+    "revision": "507788f24985862ae3e24aa43312c707",
+    "url": "/robofriends/index.html"
+  }
+];
